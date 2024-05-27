ea2f3516e23717195a04e237140cd9f0b173b54e
KDH1029 <dohappy1029@gmail.com>
2024-05-27 15:40:10 +0800

    main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ app.set('views', './views');
 app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
-  res.render('templete', {나연이:"메롱",너구리:"바보"});
+  res.render('templete', {file:"main"});
 });
 
 app.listen(port, () => {
