92c656e0df1b9542bba4f327070e9afd6101a7d7
KNY <nahappy1029@gmail.com>
2024-06-05 12:25:10 +0800

    commit

---
+++ src/script/menu.js
@@ -0,0 +1,52 @@
+/*
+babyIcePrice ¥
+10
++(babyiceflavor1-1 + babyiceflavor1-2 + babyiceflavor1-3)*9
++(babyicetopping1-1 + babyicetopping1-2 + babyicetopping1-3 + babyicetopping1-4
+    + babyicetopping1-5 + babyicetopping1-6 + babyicetopping1-7)*3
+
+iceIceBabyPrice ¥
+20
++(iceicebabyflavor1-1 + iceicebabyflavor1-2 + iceicebabyflavor1-3)*9
++(iceicebabytopping1-1 + iceicebabytopping1-2 + iceicebabytopping1-3 + iceicebabytopping1-4
+    + iceicebabytopping1-5 + iceicebabytopping1-6 + iceicebabytopping1-7)*3
+
+bigIceBabyPrice ¥
+30
++(bigicebabyflavor1-1 + bigicebabyflavor1-2 + bigicebabyflavor1-3)*9
++(bigicebabytopping1-1 + bigicebabytopping1-2 + bigicebabytopping1-3 + bigicebabytopping1-4
+    + bigicebabytopping1-5 + bigicebabytopping1-6 + bigicebabytopping1-7)*3
+
+iceWaffleBabyPrice ¥
+32
++(icewafflebabyflavor1-1 + icewafflebabyflavor1-2 + icewafflebabyflavor1-3)*9
++(icewafflebabytopping1-1 + icewafflebabytopping1-2 + icewafflebabytopping1-3 + icewafflebabytopping1-4
+    + icewafflebabytopping1-5 + icewafflebabytopping1-6 + icewafflebabytopping1-7)*3
+
+iceAffogatoBabyPrice ¥
+18
++(iceaffogatobabyflavor1-1 + iceaffogatobabyflavor1-2 + iceaffogatobabyflavor1-3)*9
++(iceaffogatobabytopping1-1 + iceaffogatobabytopping1-2 + iceaffogatobabytopping1-3 + iceaffogatobabytopping1-4
+    + iceaffogatobabytopping1-5 + iceaffogatobabytopping1-6 + iceaffogatobabytopping1-7)*3
+
+bigIceBabyPrice ¥
+30
++(bigicebabyflavor1-1 + bigicebabyflavor1-2 + bigicebabyflavor1-3)*9
++(bigicebabytopping-1 + bigicebabytopping-2 + bigicebabytopping-3 + bigicebabytopping-4
+    + bigicebabytopping-5 + bigicebabytopping-6 + bigicebabytopping-7)*3
+
+icePrice ¥
+(iceflavor1-1 + iceflavor1-2 + iceflavor1-3)*9
++(icetopping1-1 + icetopping1-2 + icetopping1-3 + icetopping1-4
+    + icetopping1-5 + icetopping1-6 + icetopping1-7)*3
+
+topPrice ¥
+(toptopping1-1 + toptopping1-2 + toptopping1-3 + toptopping1-4
+    + toptopping1-5 + toptopping1-6 + toptopping1-7)*3
+
+coffeePrice ¥
+(coffee1-1 + coffee1-2)*8
+
+waterPrice ¥
+water1*2
+
