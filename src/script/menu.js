51b4d4315b50c813e3316493616992d363ed7745
KDH1029 <dohappy1029@gmail.com>
2024-06-12 15:12:57 +0800

    .

--- src/script/menu.js
+++ src/script/menu.js
@@ -1,7 +1,8 @@
 var defaultPrice = 0, price = 0;
+var menu;
 window.onload = () => {
   ["addBI", "addIIB", "addIWB", "addIAB", "addBIB", "addEI", "addET", "addC", "addW"].forEach(e => { document.getElementById(e).addEventListener('click', () => showAdd(e)); });
-  document.getElementById('button').addEventListener('click', () => Add());
+  document.getElementById('button').addEventListener('click', () => addCart(menu));
   ["iceflavor1-1","iceflavor1-2","iceflavor1-3","toptopping1-1","toptopping1-2","toptopping1-3","toptopping1-3","toptopping1-5","toptopping1-6","toptopping1-7","coffee1-1","coffee1-2","water1","flavor1-1","flavor1-2","flavor1-3","topping1-1","topping1-2","topping1-3","topping1-4","topping1-5","topping1-6","topping1-7"].forEach(e => { document.getElementById(e).addEventListener('change', () => setPrice(e)); });
 }
 function showAdd(btn) {
@@ -12,14 +13,7 @@ function showAdd(btn) {
       document.getElementById('Label').textContent = "Baby Ice";
       defaultPrice = 10; document.getElementById('Price').textContent = defaultPrice;
       ["combomain", "flavor", "topping", "icecream-1", "topping-1", "combala", "accordionice"].forEach(e => show(e));
-      addCart(new Order("BabyIce",[Number(document.getElementById("flavor1").value),0,0],
-        [Number(document.getElementById("topping1").value),0,0],
-        [(Number(document.getElementById("flavor1-1").value)+Number(document.getElementById("iceflavor1-1").value)),(Number(document.getElementById("flavor1-2").value)+Number(document.getElementById("iceflavor1-2").value)),(Number(document.getElementById("flavor1-3").value)+Number(document.getElementById("iceflavor1-3").value))],
-        [(Number(document.getElementById("topping1-1").value)+Number(document.getElementById("toptopping1-1").value)),(Number(document.getElementById("topping1-2").value)+Number(document.getElementById("toptopping1-2").value)),(Number(document.getElementById("topping1-3").value)+Number(document.getElementById("toptopping1-3").value)),(Number(document.getElementById("topping1-4").value)+Number(document.getElementById("toptopping1-4").value)),(Number(document.getElementById("topping1-5").value)+Number(document.getElementById("toptopping1-5").value)),(Number(document.getElementById("topping1-6").value)+Number(document.getElementById("toptopping1-6").value)),(Number(document.getElementById("topping1-7").value)+Number(document.getElementById("toptopping1-7").value))],
-        [0,0],
-        [0],
-        [0],
-        Number(document.getElementById("Price").value)));
+      menu = new Order("BabyIce",[null],[null])
       break;
     case "addIIB":
       document.getElementById('Label').textContent = "Ice Ice Baby";
@@ -134,8 +128,11 @@ function setPrice() { price = defaultPrice
   + (Number(document.getElementById("flavor1-1").value)+Number(document.getElementById("flavor1-2").value)+Number(document.getElementById("flavor1-3").value) + Number(document.getElementById("iceflavor1-1").value)+Number(document.getElementById("iceflavor1-2").value)+Number(document.getElementById("iceflavor1-3").value))*9
   + (Number(document.getElementById("topping1-1").value)+Number(document.getElementById("topping1-2").value)+Number(document.getElementById("topping1-3").value)+Number(document.getElementById("topping1-4").value)+Number(document.getElementById("topping1-5").value)+Number(document.getElementById("topping1-6").value)+Number(document.getElementById("topping1-7").value) + Number(document.getElementById("toptopping1-1").value)+Number(document.getElementById("toptopping1-2").value)+Number(document.getElementById("toptopping1-3").value)+Number(document.getElementById("toptopping1-4").value)+Number(document.getElementById("toptopping1-5").value)+Number(document.getElementById("toptopping1-6").value)+Number(document.getElementById("toptopping1-7").value))*3
   + (Number(document.getElementById("coffee1-1").value)+Number(document.getElementById("coffee1-2").value))*8
-  + (Number(document.getElementById("water1").value))*2
-  ; document.getElementById("Price").textContent = price; }
+  + (Number(document.getElementById("water1").value))*2;
+  document.getElementById("Price").textContent = price;
+//여기여기여기 menu.뭐시기 = 저시기
+//ex) menu.iceNum=[0,0,0];
+}
 function show(id) { document.getElementById(id).style.display = ""; }
 function hide(id) { document.getElementById(id).style.display = "none"; }
 
