12896a85e45162b03fad63f7e44e9fce4fdf11f9
KNY <nahappy1029@gmail.com>
2024-06-14 01:44:52 +0800

    c

--- src/script/menu.js
+++ src/script/menu.js
@@ -127,34 +127,34 @@ ${temp.iceNum[0] + temp.iceNum[1] + temp.iceNum[2] + temp.iceAdd[0] + temp.iceAd
     <button type="button" class="btn-close" data-bs-dismiss="card" aria-label="Close" id="productclose" onclick="javasript:this.parentNode.parentNode.remove();totalMenu(-1*Number(this.parentNode.parentNode.querySelector('span').textContent));"></button>
   </div>
   <div class="card-body">
-    ${temp.iceNum[0] + temp.iceNum[1] + temp.iceNum[2] != 0 ? `
+    ${temp.iceNum[0] + temp.iceNum[1] + temp.iceNum[2] + temp.iceAdd[0] + temp.iceAdd[1] + temp.iceAdd[2] != 0 ? `
     <div class="card" id="producticecream">
       <div class="card-body">
         <h5 class="card-title">Ice Cream</h5>
         <div class="row">
     `: ''}
-          ${temp.iceNum[0] != 0 ? `<div class="col"><p class="card-text" id="productflavor1">Vanilla: <span id="productFlavor1">${temp.iceNum[0]}</span></p></div>` : ''}
-          ${temp.iceNum[1] != 0 ? `<div class="col"><p class="card-text" id="productflavor2">Chocolate: <span id="productFlavor2">${temp.iceNum[1]}</span></p></div>` : ''}
-          ${temp.iceNum[2] != 0 ? `<div class="col"><p class="card-text" id="productflavor3">Strawberry: <span id="productFlavor3">${temp.iceNum[2]}</span></p></div>` : ''}
-    ${temp.iceNum[0] + temp.iceNum[1] + temp.iceNum[2] != 0 ? `
+          ${temp.iceNum[0] + temp.iceAdd[0] != 0 ? `<div class="col"><p class="card-text" id="productflavor1">Vanilla: <span id="productFlavor1">${temp.iceNum[0] + temp.iceAdd[0]}</span></p></div>` : ''}
+          ${temp.iceNum[1] + temp.iceAdd[1] != 0 ? `<div class="col"><p class="card-text" id="productflavor2">Chocolate: <span id="productFlavor2">${temp.iceNum[1] + temp.iceAdd[1]}</span></p></div>` : ''}
+          ${temp.iceNum[2] + temp.iceAdd[2] != 0 ? `<div class="col"><p class="card-text" id="productflavor3">Strawberry: <span id="productFlavor3">${temp.iceNum[2] + temp.iceAdd[2]}</span></p></div>` : ''}
+    ${temp.iceNum[0] + temp.iceNum[1] + temp.iceNum[2] + temp.iceAdd[0] + temp.iceAdd[1] + temp.iceAdd[2] != 0 ? `
         </div>
       </div>
     </div>
     `: ''}
-    ${temp.topNum[0] + temp.topNum[1] + temp.topNum[2] + temp.topNum[3] + temp.topNum[4] + temp.topNum[5] + temp.topNum[6] != 0 ? `
+    ${temp.topNum[0] + temp.topNum[1] + temp.topNum[2] + temp.topNum[3] + temp.topNum[4] + temp.topNum[5] + temp.topNum[6] + temp.topAdd[0] + temp.topAdd[1] + temp.topAdd[2] + temp.topAdd[3] + temp.topAdd[4] + temp.topAdd[5] + temp.topAdd[6] != 0 ? `
     <div class="card" id="producttoppings">
       <div class="card-body">
         <h5 class="card-title">Toppings</h5>
         <div class="row">
     `: ''}
-          ${temp.topNum[0] != 0 ? `<div class="col-6"><p class="card-text" id="producttop1">Oreos: <span id="productTop1">${temp.topNum[0]}</span></p></div>` : ''}
-          ${temp.topNum[1] != 0 ? `<div class="col-6"><p class="card-text" id="producttop2">Sprinkle: <span id="productTop2">${temp.topNum[1]}</span></p></div>` : ''}
-          ${temp.topNum[2] != 0 ? `<div class="col-6"><p class="card-text" id="producttop3">Fudge Syrup: <span id="productTop3">${temp.topNum[2]}</span></p></div>` : ''}
-          ${temp.topNum[3] != 0 ? `<div class="col-6"><p class="card-text" id="producttop4">Caramel Syrup: <span id="productTop4">${temp.topNum[3]}</span></p></div>` : ''}
-          ${temp.topNum[4] != 0 ? `<div class="col-6"><p class="card-text" id="producttop5">Waffle Bit: <span id="productTop5">${temp.topNum[4]}</span></p></div>` : ''}
-          ${temp.topNum[5] != 0 ? `<div class="col-6"><p class="card-text" id="producttop6">Waffle Cone: <span id="productTop6">${temp.topNum[5]}</span></p></div>` : ''}
-          ${temp.topNum[6] != 0 ? `<div class="col-12"><p class="card-text" id="producttop7">Whipped Topping: <span id="productTop7">${temp.topNum[6]}</span></p></div>` : ''}
-    ${temp.topNum[0] + temp.topNum[1] + temp.topNum[2] + temp.topNum[3] + temp.topNum[4] + temp.topNum[5] + temp.topNum[6] != 0 ? `
+          ${temp.topNum[0] + temp.topAdd[0] != 0 ? `<div class="col-6"><p class="card-text" id="producttop1">Oreos: <span id="productTop1">${temp.topNum[0] + temp.topAdd[0]}</span></p></div>` : ''}
+          ${temp.topNum[1] + temp.topAdd[1] != 0 ? `<div class="col-6"><p class="card-text" id="producttop2">Sprinkle: <span id="productTop2">${temp.topNum[1] + temp.topAdd[1]}</span></p></div>` : ''}
+          ${temp.topNum[2] + temp.topAdd[2] != 0 ? `<div class="col-6"><p class="card-text" id="producttop3">Fudge Syrup: <span id="productTop3">${temp.topNum[2] + temp.topAdd[2]}</span></p></div>` : ''}
+          ${temp.topNum[3] + temp.topAdd[3] != 0 ? `<div class="col-6"><p class="card-text" id="producttop4">Caramel Syrup: <span id="productTop4">${temp.topNum[3] + temp.topAdd[3]}</span></p></div>` : ''}
+          ${temp.topNum[4] + temp.topAdd[4] != 0 ? `<div class="col-6"><p class="card-text" id="producttop5">Waffle Bit: <span id="productTop5">${temp.topNum[4] + temp.topAdd[4]}</span></p></div>` : ''}
+          ${temp.topNum[5] + temp.topAdd[5] != 0 ? `<div class="col-6"><p class="card-text" id="producttop6">Waffle Cone: <span id="productTop6">${temp.topNum[5] + temp.topAdd[5]}</span></p></div>` : ''}
+          ${temp.topNum[6] + temp.topAdd[6] != 0 ? `<div class="col-12"><p class="card-text" id="producttop7">Whipped Topping: <span id="productTop7">${temp.topNum[6] + temp.topAdd[6]}</span></p></div>` : ''}
+    ${temp.topNum[0] + temp.topNum[1] + temp.topNum[2] + temp.topNum[3] + temp.topNum[4] + temp.topNum[5] + temp.topNum[6] + temp.topAdd[0] + temp.topAdd[1] + temp.topAdd[2] + temp.topAdd[3] + temp.topAdd[4] + temp.topAdd[5] + temp.topAdd[6] != 0 ? `
         </div>
       </div>
     </div>
